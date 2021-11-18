db4467f2409b714e08d4feadc8dc5d024477d7fa
carracino <71412968+carracino@users.noreply.github.com>
2021-11-18 12:39:10 -0500

    Add files via upload

---
+++ Intune-Sync.ps1
@@ -0,0 +1,1 @@
+﻿# Force a MDM sync locally $Shell = New-Object -ComObject Shell.Application$Shell.open("intunemanagementextension://syncapp")
---
+++ SAM-nightmare-fix.ps1
@@ -0,0 +1,48 @@
+﻿#Intune Proactive Remediation to resolve SAM database permission LPE issue
+
+#detection:
+$detection = icacls C:\windows\System32\config\SAM
+
+If ($detection -like "*BUILTIN\Users:(I)(RX)*")
+{
+return $False
+}
+Else
+{
+return $True
+}
+
+#remediation:
+
+#First check status on restore points: If none found, then wont create new RP.
+$restorepoints = Get-ComputerRestorePoint
+    If ($restorepoints.count -eq 0)
+    {
+    $CreateNew = $False
+    }
+
+#fix acl on SAM dir:
+$SAM = icacls C:\windows\System32\config\*.* /inheritance:e
+
+#remove all shadow copies
+$removeShadows = vssadmin delete shadows /for=c: /all /Quiet
+
+#check for any remains of restore points:
+$restorepoints = Get-ComputerRestorePoint
+
+#confirm removal:
+If ($restorepoints.count -eq 0)
+{
+#compliant
+    #try to create new restore point:
+    Try{
+        If ($CreateNew -ne $False)
+        {
+        Checkpoint-Computer -Description "SAM-Fix"
+        }
+    }
+    Catch
+    {
+    write-host $Error[0]
+    }
+}
